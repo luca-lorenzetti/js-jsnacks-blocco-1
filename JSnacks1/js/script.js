13d29a5ac8487330e77f7b4858587d1f8c0fe2e0
luca-lorenzetti <77396718+luca-lorenzetti@users.noreply.github.com>
2021-03-02 11:19:54 +0100

    Add files via upload

---
+++ JSnacks1/js/script.js
@@ -0,0 +1,14 @@
+var num1 = prompt("Inserire il primo numero");
+var num2 = prompt("Inserire il secondo numero");
+
+if( num1 > num2){
+    alert("Il numero maggiore è " + num1);
+}
+
+else if( num1 == num2){
+    alert("I numeri sono uguali ");
+}
+else{
+    alert("Il numero maggiore è " + num2);
+    
+}
