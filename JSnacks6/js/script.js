7189158008d200b90b5209804d54c5a148979486
luca <luca.lorenzetti0@gmail.com>
2021-03-03 11:56:26 +0100

    Fix stringa

--- JSnacks6/js/script.js
+++ JSnacks6/js/script.js
@@ -1,4 +1,4 @@
-var num = parseInt(prompt("Vuoi calcolare i cubo di quanti numeri?") );
+var num = parseInt(prompt("Vuoi calcolare il cubo di quanti numeri?") );
 
 for (var i = 1; i <= num; i++) {
     console.log("Il cubo di " + i +" e': " + Math.pow(i,3) );    
